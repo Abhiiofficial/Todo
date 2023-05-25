c173956cfc4311548781b3c0ed4e066759e5d266
Abhishek <you@example.com>
2023-05-25 12:04:47 +0530

    bug fixing2

--- src/Pages/TodoList/TodoList.tsx
+++ src/Pages/TodoList/TodoList.tsx
@@ -2,8 +2,10 @@ import './TodoList.css'
 import TodoLogin from '../../Components/TodoLogin/TodoLogin'
 import TodoSignUp from '../../Components/TodoSignUp/TodoSignUp'
 import Todo from '../../Components/Todo/Todo'
-import { useState } from 'react'
+import {  useState } from 'react'
 import FooterRow from '../../Components/Footer/Footer'
+import {LazyLoadImage}  from 'react-lazy-load-image-component';
+import image from '../../Assets/bgimage.png'
 
 const TodoList = () => {
   const [mode, setMode] = useState<string>("login")
@@ -11,10 +13,17 @@ const TodoList = () => {
   const getMode = (data: string) => {
     setMode(data)
   }
+
   return (
+
     <div className='todo'>
       <div className="bg-container">
-        <img src='https://i.pinimg.com/originals/30/f4/0c/30f40c02566f9981446a10d52ece94bd.png' alt="" className="bg-image" />
+        <LazyLoadImage
+          alt='bg'
+          src={image} 
+          className="bg-image"
+          />
+        {/* <img src={LazyImage} alt="" className="bg-image" /> */}
       </div>
       {mode === 'login' && <TodoLogin getMode={getMode} />}
       {mode === 'signup' && <TodoSignUp getMode={getMode} />}
